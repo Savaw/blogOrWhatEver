07ae40dc346dd28a560ca7c0aae72e198fea52dd
savaw <savaw97@gmail.com>
2019-08-03 11:49:18 +0430

    display posts

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Saba/Desktop/awsomeTwitter/ENV"
+set -gx VIRTUAL_ENV "/Users/Saba/Desktop/djangoBlog/ENV"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
